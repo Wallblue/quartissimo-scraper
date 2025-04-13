f4fdc99674585add3d6d766b62b948e66ca5a28a
Wallblue <nrb.busquet@gmail.com>
2025-04-13 15:51:17 +0200

    Create Scraper abstract class

--- src/main/java/org/quartissimo/scrapapp/scraper/Scraper.java
+++ src/main/java/org/quartissimo/scrapapp/scraper/Scraper.java
@@ -2,10 +2,13 @@ package org.quartissimo.scrapapp.scraper;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 public abstract class Scraper {
@@ -31,9 +34,9 @@ public abstract class Scraper {
         this.maxWaitingTime = maxWaitingTime;
     }
 
-    public void waiting() throws InterruptedException {
+    public void waiting(int min, int max) throws InterruptedException {
         Random rand = new Random();
-        Thread.sleep(this.minWaitingTime + rand.nextInt(this.maxWaitingTime));
+        Thread.sleep(min + rand.nextInt(max - min));
     }
 
     protected void loadPage(String url, By elementToWaitFor){
@@ -44,6 +47,23 @@ public abstract class Scraper {
 
     public void setDriver(WebDriver driver) {
         this.driver = driver;
+    }
+
+    // This methods allow us to look for an element that could be absent without crashing
+    protected Optional<WebElement> safeFindElement(By by){
+        List<WebElement> found = this.driver.findElements(by);
+        return found.isEmpty() ? Optional.empty() : Optional.of(found.getFirst());
+    }
+
+    protected Optional<WebElement> safeFindElementInElement(WebElement sourceElement, By by){
+        List<WebElement> found = this.driver.findElements(by);
+        return found.isEmpty() ? Optional.empty() : Optional.of(found.getFirst());
+    }
+
+    protected void closeTab(){
+        Object[] windowHandles = this.driver.getWindowHandles().toArray();
+        this.driver.close();
+        this.driver.switchTo().window((String) windowHandles[0]);
     }
 
     public abstract void scrape() throws Exception;
